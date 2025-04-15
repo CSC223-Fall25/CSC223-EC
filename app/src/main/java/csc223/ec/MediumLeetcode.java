93ad07420d09ce1e87ebf8ad00e829020497d82d
Jacobfulp777 <jacobfulp777@gmail.com>
2025-04-15 09:55:05 -0400

    medium done

--- app/src/main/java/csc223/ec/MediumLeetcode.java
+++ app/src/main/java/csc223/ec/MediumLeetcode.java
@@ -3,5 +3,29 @@ package csc223.ec;
 //https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/description/
 
 public class MediumLeetcode {
+    class Solution {
+   
+
+        public int sumEvenGrandparent(TreeNode root) {
+            //initialize DFS
+            int sum = 0;
+            depthFirst(root, null, null);
+            return sum; 
+        }
     
+        public void depthFirst(TreeNode root, Integer parent, Integer grandParent){
+            //base case
+            if(root == null) return 0;
+    
+            // if grandparent node is even, add curr nodes value to the sum
+            if(grandParent != null){
+                if(grandParent % 2 == 0){
+                    sum += root.val;
+                }
+            }
+            //recursive call to left and right children
+            depthFirst(root.left, root.val, parent);
+            depthFirst(root.right, root.val, parent);
+        }
+    }
 }
