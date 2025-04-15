252fb44dae0a2ce861a3d7027e43228318268b0b
EmilyHattenn <eshatten@bellsouth.net>
2025-04-15 11:14:17 -0400

    Emily's Last Contribution for Lab 8

--- app/src/main/java/csc223/ec/MediumLeetcode.java
+++ app/src/main/java/csc223/ec/MediumLeetcode.java
@@ -3,29 +3,50 @@ package csc223.ec;
 //https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/description/
 
 public class MediumLeetcode {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val; 
+        }
+        
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     class Solution {
-   
+        private int sum;
 
         public int sumEvenGrandparent(TreeNode root) {
             //initialize DFS
-            int sum = 0;
-            depthFirst(root, null, null);
+            sum = 0;
+            if (root != null) {
+                depthFirst(root.left, root, null);
+                depthFirst(root.right, root, null);
+            }
+           
             return sum; 
         }
     
-        public void depthFirst(TreeNode root, Integer parent, Integer grandParent){
+        public void depthFirst(TreeNode current, TreeNode parent, TreeNode grandParent){
             //base case
-            if(root == null) return 0;
+            if (current == null) return;
     
             // if grandparent node is even, add curr nodes value to the sum
-            if(grandParent != null){
-                if(grandParent % 2 == 0){
-                    sum += root.val;
-                }
+            if (grandParent != null && grandParent.val % 2 == 0){
+                this.sum += current.val;
             }
             //recursive call to left and right children
-            depthFirst(root.left, root.val, parent);
-            depthFirst(root.right, root.val, parent);
+            depthFirst(current.left, current, parent);
+            depthFirst(current.right, current, parent);
         }
     }
 }
