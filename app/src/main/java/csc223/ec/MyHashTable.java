996be57768cf8c0edd32bc9d58653aa572d52912
ElliottChmil <elliottchil@gmail.com>
2025-04-23 14:33:11 -0400

    MyHashTable first

---
+++ app/src/main/java/csc223/ec/MyHashTable.java
@@ -0,0 +1,29 @@
+package csc223.ec;
+
+public class MyHashTable {
+    //Inserts or updates a key-value pair.
+    public void put(String key, Integer value) {
+
+    }
+
+    //Retrieves the value associated with the key, or null if not found.
+    public void get(String key) {
+
+    } 
+
+    //Deletes the key and its associated value.
+    public void remove(String key) {
+
+    }
+
+    //Returns true if the key exists, false otherwise.
+    public void containsKey(String key) {
+
+    }
+
+    //Returns the number of key-value pairs in the table.
+    public void size() {
+
+    }
+
+}
