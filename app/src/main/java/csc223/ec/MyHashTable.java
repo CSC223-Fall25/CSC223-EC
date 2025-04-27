82d342ff00c326c9794beefe1259a1ac52bf7ebd
ElliottChmil <elliottchil@gmail.com>
2025-04-27 16:00:22 -0400

    Hashtable untested

--- app/src/main/java/csc223/ec/MyHashTable.java
+++ app/src/main/java/csc223/ec/MyHashTable.java
@@ -1,18 +1,32 @@
 package csc223.ec;
 import java.util.ArrayList;
-import csc223.ec.HashTuple;
 
 public class MyHashTable {
     ArrayList<HashTuple>[] table;
     int capacity;
     
+    public MyHashTable(int capacity) {
+        this.table = new ArrayList[capacity];
+        this.capacity = capacity;
+
+        //initialize the arraylists
+        for (int i=0; i< capacity; i++) {
+            this.table[i] = new ArrayList<HashTuple>();
+        }
+    }
+
     public MyHashTable() {
         this.table = new ArrayList[10];
         this.capacity = 10;
+
+        //initialize the arraylists
+        for (int i=0; i< capacity; i++) {
+            this.table[i] = new ArrayList<HashTuple>();
+        }
     }
 
 
-    //Inserts or updates a key-value pair.
+    //Inserts or updates a key-value pair. (Doesn't return anything)
     public void put(String key, Integer value) {
         int index = Math.abs(key.hashCode()) % this.capacity;
         if (!containsKey(key)) {
@@ -30,7 +44,7 @@ public class MyHashTable {
         
     }
 
-    //Retrieves the value associated with the key, or null if not found.
+    //Retrieves the value associated with the key, or null if not found. (If not found returns -Max int)
     public int get(String key) {
         int index = Math.abs(key.hashCode()) % this.capacity;
         for (HashTuple tuple : this.table[index]) {
@@ -38,13 +52,26 @@ public class MyHashTable {
                 return tuple.value;
             }
         }
-        //fix this idk what to return
-        return -1;
+        //return -max value instead of null
+        return -Integer.MAX_VALUE;
     } 
 
-    //Deletes the key and its associated value.
+    //Deletes the key and its associated value. (Doesn't return anything)
     public void remove(String key) {
+        int index = Math.abs(key.hashCode()) % this.capacity;
+        if (!this.containsKey(key)) {
+            System.out.println("Error, Key does not exist");
+            return;
+        }
 
+        //search for item and delete it.
+        ArrayList<HashTuple> bucket = this.table[index];
+        for (int idx = 0; idx < bucket.size(); idx++) {
+            if (bucket.get(idx).key.equals(key)) {
+                bucket.remove(idx);
+                return;
+            }
+        }
     }
 
     //Returns true if the key exists, false otherwise.
@@ -64,8 +91,13 @@ public class MyHashTable {
     }
 
     //Returns the number of key-value pairs in the table.
-    public void size() {
-
+    public int size() {
+        int numPairs = 0;
+        for (int bucketIdx = 0; bucketIdx < this.table.length; bucketIdx++) {
+            ArrayList<HashTuple> bucket = this.table[bucketIdx];
+            numPairs += bucket.size();
+        }
+        return numPairs;
     }
 
 }
